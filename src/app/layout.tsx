7c1fd954a668205dab583f40aa4b3f7b2fdb0bcc
Andrii Yusyp <serafimsanvol@gmail.com>
2024-04-03 18:23:48 +0300

    Add clarity tracking

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import './globals.css';
+import Script from 'next/script';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -17,6 +18,13 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${inter.className} mx-auto max-w-7xl`}>{children}</body>
+      <Script id="clarity">
+        {`(function(c,l,a,r,i,t,y){
+        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+    })(window, document, "clarity", "script", "lqvong8em9");`}
+      </Script>
     </html>
   );
 }
