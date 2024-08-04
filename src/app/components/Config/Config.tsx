4df1f9c9d3a8d7c8652a692415443a4e0847a08a
Andrii Yusyp <serafimsanvol@gmail.com>
2024-08-04 20:54:19 +0300

    Fix build

--- src/app/components/Config/Config.tsx
+++ src/app/components/Config/Config.tsx
@@ -6,49 +6,50 @@ import {
 } from './constants';
 
 const Config = ({ extension }: { extension: SaveExtensionFormatsType }) => {
-  const options = {} as SaveBufferOptions;
-  const isLossless = options && 'lossless' in options;
+  return null;
+  // const options = {} as SaveBufferOptions;
+  // const isLossless = options && 'lossless' in options;
 
-  return (
-    <div>
-      {isLossless && options.lossless && (
-        <div className="form-control">
-          <label className="label cursor-pointer">
-            <span className="label-text">Lossless</span>
-            <input
-              className="checkbox"
-              type="checkbox"
-              name="lossless"
-              id="lossless"
-            />
-          </label>
-        </div>
-      )}
-      <p className="text-center mb-2">Quality</p>
-      <input
-        type="range"
-        name="quality"
-        min={0}
-        max={100}
-        className="range range-lg"
-      />
-      <div className="w-full mb-2 flex justify-between text-xs px-2">
-        <span>Lowest</span>
-        <span>Highest</span>
-      </div>
-      <p className="text-center mb-2">Format</p>
-      <select
-        defaultValue={extension}
-        className="select select-bordered w-full mb-4"
-      >
-        {Object.keys(SaveExtensionFormats).map((key) => (
-          <option key={key} value={key}>
-            {key}
-          </option>
-        ))}
-      </select>
-    </div>
-  );
+  // return (
+  //   <div>
+  //     {isLossless && options.lossless && (
+  //       <div className="form-control">
+  //         <label className="label cursor-pointer">
+  //           <span className="label-text">Lossless</span>
+  //           <input
+  //             className="checkbox"
+  //             type="checkbox"
+  //             name="lossless"
+  //             id="lossless"
+  //           />
+  //         </label>
+  //       </div>
+  //     )}
+  //     <p className="text-center mb-2">Quality</p>
+  //     <input
+  //       type="range"
+  //       name="quality"
+  //       min={0}
+  //       max={100}
+  //       className="range range-lg"
+  //     />
+  //     <div className="w-full mb-2 flex justify-between text-xs px-2">
+  //       <span>Lowest</span>
+  //       <span>Highest</span>
+  //     </div>
+  //     <p className="text-center mb-2">Format</p>
+  //     <select
+  //       defaultValue={extension}
+  //       className="select select-bordered w-full mb-4"
+  //     >
+  //       {Object.keys(SaveExtensionFormats).map((key) => (
+  //         <option key={key} value={key}>
+  //           {key}
+  //         </option>
+  //       ))}
+  //     </select>
+  //   </div>
+  // );
 };
 
 export default Config;
