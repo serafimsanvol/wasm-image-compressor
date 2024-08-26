1ce5f7fb42afc2e7672bdc865d7512903cb65674
Andrii Yusyp <serafimsanvol@gmail.com>
2024-08-26 16:42:00 +0300

    Fix DropArea input click detection

--- src/app/components/DropArea.tsx
+++ src/app/components/DropArea.tsx
@@ -19,10 +19,10 @@ const DropArea = ({
       onDragEnd={preventDefaultAndStopPropagation}
       className="flex cursor-pointer w-full mx-auto justify-center items-center min-h-[80vh] max-w-[80vw] bg-primary card shadow-xl"
     >
-      <p className="text-white absolute left-[50%] top-[50%] -translate-y-1/2 -translate-x-1/2">
-        Drop your files here
-      </p>
       <label htmlFor="fileInput" className="w-full grow">
+        <p className="text-white absolute left-[50%] top-[50%] -translate-y-1/2 -translate-x-1/2">
+          Drop your files here
+        </p>
         <input
           className="text-white file-input bg-transparent w-full invisible"
           id="fileInput"
